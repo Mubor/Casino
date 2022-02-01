c60d0111d732597d5faa4cd93ce7b4852a5314af
Mubor <muhatasov22@ukr.net>
2022-02-01 03:37:17 +0200

    removed style problems

--- js/formUpdating.js
+++ js/formUpdating.js
@@ -1,16 +1,16 @@
-//Обновление текста на странице
+// Обновление текста на странице
+// eslint-disable-next-line no-unused-vars
 function updateForm(messagesObj, isFieldVisibile) {
-    let field = document.getElementById("bet");
+  const field = document.getElementById('bet');
 
-    if(isFieldVisibile) {
-        field.style.display = "unset";
-    }
-    else {
-        field.style.display = "none";
-    }
+  if (isFieldVisibile) {
+    field.style.display = 'unset';
+  } else {
+    field.style.display = 'none';
+  }
 
-    field.innerHTML = "";
-    document.getElementById("wallet").innerHTML = messagesObj.wallet;
-    document.getElementById("game-msg").innerHTML = messagesObj.msg;
-    document.getElementById("toss-button").innerHTML = messagesObj.button;
+  field.innerHTML = '';
+  document.getElementById('wallet').innerHTML = messagesObj.wallet;
+  document.getElementById('game-msg').innerHTML = messagesObj.msg;
+  document.getElementById('toss-button').innerHTML = messagesObj.button;
 }
